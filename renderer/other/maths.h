95a6b95e2d359831cfb16aa1e46a53bb1c922841
rabbitfishy <144937211+rabbitfishy@users.noreply.github.com>
2023-12-19 20:35:32 +0000

    Update maths.h

--- renderer/other/maths.h
+++ renderer/other/maths.h
@@ -48,6 +48,95 @@ public:
 	int w, h;
 };
 
+class vector_2d
+{
+public:
+	constexpr vector_2d() : x{ }, y{ } { }
+	constexpr vector_2d(const float x, const float y) : x{ x }, y{ y } { }
+
+	// index operators.
+	[[nodiscard]] float& operator[](const std::size_t index) { return reinterpret_cast<float*>(this)[index]; }
+	[[nodiscard]] const float& operator[](const std::size_t index) const { return reinterpret_cast<const float*>(this)[index]; }
+
+	// equality operators.
+	bool operator==(const vector_2d& v) const { return v.x == this->x && v.y == this->y; }
+	bool operator!=(const vector_2d& v) const { return v.x != this->x || v.y != this->y; }
+
+	// copy assignment.
+	constexpr vector_2d& operator=(const vector_2d& v)
+	{
+		this->x = v.x; this->y = v.y;
+		return *this;
+	}
+
+	// negation-operator.
+	constexpr vector_2d operator-() const { return vector_2d{ -this->x, -this->y }; }
+
+	// arithmetic operators.
+	vector_2d operator+(const vector_2d& v) const { return { this->x + v.x, this->y + v.y }; }
+	vector_2d operator-(const vector_2d& v) const { return { this->x - v.x, this->y - v.y }; }
+	vector_2d operator*(const vector_2d& v) const { return { this->x * v.x, this->y * v.y }; }
+	vector_2d operator/(const vector_2d& v) const { return { this->x / v.x, this->y / v.y }; }
+
+	// compound assignment operators.
+	vector_2d operator+=(const vector_2d& v)
+	{
+		this->x += v.x; this->y += v.y;
+		return *this;
+	}
+
+	vector_2d operator-=(const vector_2d& v)
+	{
+		this->x -= v.x; this->y -= v.y;
+		return *this;
+	}
+
+	vector_2d operator*=(const vector_2d& v)
+	{
+		this->x *= v.x; this->y *= v.y;
+		return *this;
+	}
+
+	vector_2d operator/=(const vector_2d& v)
+	{
+		this->x /= v.x; this->y /= v.y;
+		return *this;
+	}
+
+	// arithmetic operators w/ float.
+	vector_2d operator+(const float f) const { return { this->x + f, this->y + f }; }
+	vector_2d operator-(const float f) const { return { this->x - f, this->y - f }; }
+	vector_2d operator*(const float f) const { return { this->x * f, this->y * f }; }
+	vector_2d operator/(const float f) const { return { this->x / f, this->y / f }; }
+
+	// compound assignment operators w/ float.
+	vector_2d& operator+=(float f)
+	{
+		this->x += f; this->y += f;
+		return *this;
+	}
+
+	vector_2d& operator-=(float f)
+	{
+		this->x -= f; this->y -= f;
+		return *this;
+	}
+
+	vector_2d& operator*=(float f)
+	{
+		this->x *= f; this->y *= f;
+		return *this;
+	}
+
+	vector_2d& operator/=(float f)
+	{
+		this->x /= f; this->y /= f;
+		return *this;
+	}
+
+	float x, y;
+};
+
 // NOTE: it must be float values for it to render otherwise shit won't render anything in directx.
 // that is why i put it here to use vector 2d's to render so it must be float value!!!!
 class vertex
